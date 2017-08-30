8424753e7a897fabf7f1dbb92a9a41696e4d5ae8
Amber Maillard & klorealf <pair+amber2394.klorealf@devbootcamp.com>
2017-08-30 18:21:39 -0400

    add search method and tests for search method

---
+++ binary_search.rb
@@ -0,0 +1,11 @@
+def middle_search(target, arr)
+  #i = 0
+  m = arr.length / 2
+  m
+  #until arr[m] == target
+
+
+end
+
+
+
--- spec/binary_search_spec.rb
+++ spec/binary_search_spec.rb
@@ -1,4 +1,19 @@
 require_relative '../binary_search'
 
 describe 'binary_search' do
+  let(:symbols) { [:A, :B, :C, :D, :E, :F, :G, :H, :I] }
+
+  context 'pick a point in the middle of an array' do
+    it 'returns the value of the element in the middle point of an array' do
+      index_of_e = middle_search(:E, symbols)
+      expect(index_of_e).to eq 4
+    end
+  end
+
+ context 'when the array does not contain the searched for element' do
+    it 'returns nil if the element in not in the array' do
+      index_of_x = middle_search(:X, symbols)
+      expect(index_of_x).to eq nil
+    end
+  end
 end
