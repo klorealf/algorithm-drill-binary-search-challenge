eeda4660946f10e6b1a42d486f102433c3990c53
bradsloan & Andreas Bach Landgrebe <pair+bsloan3.grebes15@devbootcamp.com>
2017-08-30 17:26:31 -0400

    got fist to run but not 2 and 3

--- binary_search.rb
+++ binary_search.rb
@@ -1,14 +1,31 @@
 require 'pry'
 def binary_search(array, value)
 # binding.pry
-  sorted_array = array.sort
-  mid = (sorted_array.length/2)
-  if value > sorted_array[mid]
-    binary_search((sorted_array[mid]..sorted_array[-1]), value)
-  elsif value < sorted_array[mid]
-    binary_search((sorted_array[0]..sorted_array[mid]), value)
+  mid = array.length/2
+  if value > array[mid]
+    return binary_search(array[mid..(array.length-1)], value)
+  elsif value < array[mid]
+    return binary_search(array[0..mid], value)
   else
     return mid
   end
 end
 
+# def binary_search(array, num)
+#   first_index = 0
+#   last_index = array.length - 1
+
+#   while first_index <= last_index
+#     mid = (first_index + last_index) / 2
+
+#     if array[mid] == num
+#       return mid
+#     elsif num < array[mid]
+#       last_index = mid - 1
+#     else
+#       first_index = mid + 1
+#     end
+#   end
+
+#   return -1
+# end
--- spec/binary_search_spec.rb
+++ spec/binary_search_spec.rb
@@ -1,13 +1,17 @@
 require_relative '../binary_search'
 
 describe 'binary_search' do
-  a = [1, 6, 7, 4, 12, 55, 9, 15, 8]
+  a = [1, 3, 6, 7, 88, 99, 100]
   it 'finds index of value through binary search' do
-    expect(binary_search(a, 7)).to eq 2
+    expect(binary_search(a, 7)).to eq 3
   end
 
-    it 'finds index of value through binary search' do
-    expect(binary_search(a, 55)).to eq 5
+  it 'finds index of value through binary search' do
+    expect(binary_search(a, 88)).to eq 4
+  end
+
+  it 'finds index of value through binary search' do
+    expect(binary_search(a, 100)).to eq 6
   end
 
 end
