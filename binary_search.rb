494237920d9437d5c1f324230f756a0fc56c75a2
bradsloan & Andreas Bach Landgrebe <pair+bsloan3.grebes15@devbootcamp.com>
2017-08-31 08:54:23 -0400

    fix binary to not be destructive

--- binary_search.rb
+++ binary_search.rb
@@ -1,23 +1,20 @@
-require 'pry'
+#require 'pry'
 def binary_search(array, value)
 # binding.pry
   mid = array.length/2
   if value > array[mid]
-    return binary_search(array[mid..(array.length-1)], value)
+    return mid + binary_search(array[mid..-1], value)
   elsif value < array[mid]
     return binary_search(array[0..mid], value)
   else
     return mid
   end
 end
-
 # def binary_search(array, num)
 #   first_index = 0
 #   last_index = array.length - 1
-
 #   while first_index <= last_index
 #     mid = (first_index + last_index) / 2
-
 #     if array[mid] == num
 #       return mid
 #     elsif num < array[mid]
@@ -26,6 +23,5 @@ end
 #       first_index = mid + 1
 #     end
 #   end
-
 #   return -1
 # end
